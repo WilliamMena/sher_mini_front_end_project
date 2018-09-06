7ee60d9eb797d60a09398e0398f5ab4ddc362033
WilliamMena <williammena1991@gmail.com>
2018-09-05 21:35:10 -0400

    Replace social media SVG's with FontAwesome icons. Deleted the javaScript functions that relate to the SVG's and delete all the SVG's from the application directory

--- functions.js
+++
@@ -1,7 +0,0 @@
-function hover(element) {
-  element.setAttribute('src', element.src.slice(0, -4)+"-white.svg");
-}
-
-function unhover(element) {
-  element.setAttribute('src', element.src.slice(0, -10)+".svg");
-}
